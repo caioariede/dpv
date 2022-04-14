1e9b1fb3f2d4afd77fe653fe0aa97cc485a92a71
Caio Ariede <caio.ariede@gmail.com>
2022-04-14 18:55:14 -0300

    fix

--- test/run_test.sh
+++ test/run_test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+export DPV_INSTALL_METHOD="$1"
+
 ./test/bats/bin/bats "test/test_$1.bats" --show-output-of-passing-tests
 ./test/bats/bin/bats "test/test.bats" --show-output-of-passing-tests
